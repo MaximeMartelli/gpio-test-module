b940e8061350d99739ae52579edaf20b79353e2b
Maxime MARTELLI <maxime.mmartelli@gmail.com>
2015-12-30 19:36:22 +0100

    ajout PWM

--- gpio-test.c
+++ gpio-test.c
@@ -3,13 +3,46 @@
 #include <linux/gpio.h>
 #include <linux/fs.h>
 
-// Sortie sur broche 18 (GPIO 24)
-#define RPI_GPIO_OUT 24
+// Sortie sur broche 12 (GPIO 18)
+#define RPI_GPIO_OUT 18
 
 // Entree sur broche 16 (GPIO 23)
 #define RPI_GPIO_IN  23
 
+#define BCM2708_PERI_BASE 	0x20000000
+#define GPIO_BASE 			(BCM2708_PERI_BASE + 0x200000) /* Controleur GPIO*/
+#define PWM_BASE 			(BCM2708_PERI_BASE + 0x20C000) /* Controleur PWM*/
+#define CLOCK_BASE 			(BCM2708_PERI_BASE + 0x101000) 
+
+#define PWM_CTL		0
+#define PWM_RNG1	4
+#define PWM_DAT1	5
+
+#define PWMCLK_CNTL	40
+#define PWMCLK_DIV	41
+
 static struct timer_list gpio_test_timer;
+
+
+static void setServo(int percent)
+{
+	int bitCount;
+	unsigned int bits = 0;
+
+	// 32 bits = 2 milliseconds
+	bitCount = 16 + 16 * percent / 100;
+	if (bitCount > 32) bitCount = 32;
+	if (bitCount < 1) bitCount = 1;
+	bits = 0;
+	while (bitCount) {
+		bits <<= 1;
+		bits |= 1;
+		bitCount--;
+	}
+	*(pwm + PWM_DAT1) = bits;
+}
+
+
 
 static void gpio_test_function (unsigned long unused)
 {
@@ -41,12 +74,47 @@ static int __init gpio_test_init (void)
     gpio_free(RPI_GPIO_IN);
     return err;
   }
-
+    
   init_timer(& gpio_test_timer);
   gpio_test_timer.function = gpio_test_function;
   gpio_test_timer.data = 0; // non utilise
   gpio_test_timer.expires = jiffies + (HZ >> 3);
   add_timer(& gpio_test_timer);
+  
+  // Active le mode PWM pour le pin 18
+  SET_GPIO_ALT(18,5);
+  // Kill clock
+  *(clk + PWMCLK_CNTL) = 0x5A000000 | (1 << 5);
+  usleep(10);
+
+	// set frequency
+	// DIVI is the integer part of the divisor
+	// the fractional part (DIVF) drops clock cycles to get the output frequency, bad for servo motors
+	// 320 bits for one cycle of 20 milliseconds = 62.5 us per bit = 16 kHz
+	int idiv = (int) (19200000.0f / 16000.0f);
+	if (idiv < 1 || idiv > 0x1000) {
+		printf("idiv out of range: %x\n", idiv);
+		exit(-1);
+	}
+	*(clk + PWMCLK_DIV)  = 0x5A000000 | (idiv<<12);
+	
+	// source=osc and enable clock
+	*(clk + PWMCLK_CNTL) = 0x5A000011;
+
+	// disable PWM
+	*(pwm + PWM_CTL) = 0;
+	
+	// needs some time until the PWM module gets disabled, without the delay the PWM module crashs
+	usleep(10);  
+	
+	// filled with 0 for 20 milliseconds = 320 bits
+	*(pwm + PWM_RNG1) = 320;
+	
+	// 32 bits = 2 milliseconds, init with 1 millisecond
+	setServo(0);
+	
+	// start PWM1 in serializer mode
+	*(pwm + PWM_CTL) = 3;
 
   return 0; 
 }
