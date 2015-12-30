89ba6a1822a807d1868d530f70e01e5f3fea1e9e
Maxime MARTELLI <maxime.mmartelli@gmail.com>
2015-12-30 12:46:32 +0100

    prout

--- gpio-test.c
+++ gpio-test.c
@@ -9,21 +9,21 @@
 // Entree sur broche 16 (GPIO 23)
 #define RPI_GPIO_IN  23
 
-static int __init rpi_gpio_1_init (void)
+static int __init gpio-test_init (void)
 {
-  printk(KERN_INFO "%s : HELLO \n", THIS_MODULE->name);
+  printk(KERN_INFO "HELLO \n");
   gpio_set_value(RPI_GPIO_OUT, 1);
   
   return 0; 
 }
 
-static void __exit rpi_gpio_1_exit (void)
+static void __exit gpio-test_exit (void)
 {
   gpio_free(RPI_GPIO_OUT);
   gpio_free(RPI_GPIO_IN);
 }
 
-module_init(rpi_gpio_1_init);
-module_exit(rpi_gpio_1_exit);
+module_init(gpio-test_init);
+module_exit(gpio-test_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Maxime MARTELLI");
